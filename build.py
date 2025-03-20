13099efcac1509aa2571126bbf8957e11f980f6f
Vertigeux <jiffyeljefe@gmail.com>
2025-03-20 12:44:46 -0400

    Prettied up build output

--- build.py
+++ build.py
@@ -8,6 +8,9 @@ setup(
     name="RatMaze",
     version="1.0",
     description="A maze for rats",
-    options={"build_exe": {"include_files": [python_dll_path, "config.ini", "resources"]}},
-    executables=[Executable("main.py")]
+    options={"build_exe": {
+        "include_files": [python_dll_path, "config.ini", "resources"],
+        "build_exe": "build/RatMaze"
+    }},
+    executables=[Executable("main.py", target_name="RatMaze.exe")]
 )
