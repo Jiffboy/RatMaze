d03bba1d08977f476bd9da9176251382fb67ad76
Vertigeux <jiffyeljefe@gmail.com>
2025-03-20 12:30:45 -0400

    Simplifying build script

--- build.py
+++ build.py
@@ -1,6 +1,5 @@
 import sys
 import os
-import shutil
 from cx_Freeze import setup, Executable
 
 python_dll_path = os.path.join(os.path.dirname(sys.executable), "python310.dll")
@@ -9,10 +8,6 @@ setup(
     name="RatMaze",
     version="1.0",
     description="A maze for rats",
-    options={"build_exe": {"include_files": [python_dll_path]}},
+    options={"build_exe": {"include_files": [python_dll_path, "config.ini", "resources"]}},
     executables=[Executable("main.py")]
 )
-
-dir = os.listdir("build")[0]
-shutil.copyfile("config.ini", f"build/{dir}/config.ini")
-shutil.copytree("resources", f"build/{dir}/resources")
